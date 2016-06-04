fc071737eb4d9b8db42430767d0f9060cb06b563
Amos Omondi <amos.omondi@andela.com>
2016-06-04 12:59:41 +0300

    Initial Commit

---
+++ helloapp/howdy/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
