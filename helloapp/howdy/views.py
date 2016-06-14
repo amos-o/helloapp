4bec4fe9d5a10d7237808d1d27698a023b3e8917
Amos Omondi <amos.omondi@andela.com>
2016-06-14 11:20:23 +0300

    (Feature) Add about template

--- helloapp/howdy/views.py
+++ helloapp/howdy/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
